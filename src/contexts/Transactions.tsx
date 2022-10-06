1e0a9c8e942c168fc54a349c045b65ecdf498a91
Tiago Castro <tiagocastro602@gmail.com>
2022-10-05 22:02:46 -0300

    migrate to axios

--- src/contexts/Transactions.tsx
+++ src/contexts/Transactions.tsx
@@ -1,4 +1,5 @@
 import { createContext, ReactNode, useEffect, useState } from 'react'
+import { api } from '../api'
 
 type Transaction = {
   id: number
@@ -24,14 +25,11 @@ export function TransactionsProvider({ children }: ProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([])
 
   const fetchTransactions = async (query?: string) => {
-    const url = new URL('http://localhost:3000/transactions')
-
-    if (query) {
-      url.searchParams.append('q', query)
-    }
-
-    const response = await fetch(url)
-    const data = await response.json()
+    const { data } = await api.get('/transactions', {
+      params: {
+        q: query,
+      },
+    })
 
     setTransactions(data)
   }
